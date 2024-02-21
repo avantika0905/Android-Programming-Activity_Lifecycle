b5e6f1394f7e854296fe12f6ef1383e803a48591
avantika0905 <140479006+avantika0905@users.noreply.github.com>
2024-02-21 11:21:40 +0530

    21/02/24

---
+++ Activity lifecycle/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "praj3224"
+include(":app")
+ 
